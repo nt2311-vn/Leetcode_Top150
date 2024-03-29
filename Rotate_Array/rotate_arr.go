1208c463660ba2d15fe6c17d778b9d0e39c10f71
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-29 21:50:26 +0700

    solve rotate arr by shifting slices

--- Rotate_Array/rotate_arr.go
+++ Rotate_Array/rotate_arr.go
@@ -1,4 +1,13 @@
 package rotate_arr
 
 func Rotate(nums []int, k int) {
+	shiftSlice := make([]int, len(nums))
+
+	for i := range nums {
+		shiftSlice[(i+k)%len(nums)] = nums[i]
+	}
+
+	for i, v := range shiftSlice {
+		nums[i] = v
+	}
 }
