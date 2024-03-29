3e197f14cf17b259380c14622cb432b75fc5c2a9
ntEOS <nguyentoan231196@gmail.com>
2024-03-29 20:03:37 +0700

    not good method

--- Rotate_Array/rotate_arr.go
+++ Rotate_Array/rotate_arr.go
@@ -1,11 +1,7 @@
 package rotate_arr
 
 func Rotate(nums []int, k int) {
-	if k < len(nums) {
-		copy(nums, append(nums[len(nums)-k:], nums[:len(nums)-k]...))
-	} else {
-		for i, j := 0, len(nums)-1; i < j; i, j = i+1, j-1 {
-			nums[i], nums[j] = nums[j], nums[i]
-		}
+	for i := 1; i <= k; i++ {
+		nums[i-1] = nums[len(nums)-i]
 	}
 }
