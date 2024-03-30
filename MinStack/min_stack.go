18cc56a1fd2a196f49210fcfd3766217a40d71a3
ntEOS <nguyentoan231196@gmail.com>
2024-03-30 15:12:39 +0700

    solve minstack

--- MinStack/min_stack.go
+++ MinStack/min_stack.go
@@ -1,20 +1,35 @@
 package min_stack
 
+import "math"
+
 type MinStack struct {
-	stack []int
+	stack      []int
+	minElement int
 }
 
-func Constructor() {
+func Constructor() MinStack {
+	minStack := MinStack{stack: []int{}, minElement: math.MaxInt64}
+	return minStack
 }
 
 func (this *MinStack) Push(val int) {
+	this.stack = append(this.stack, val)
 }
 
 func (this *MinStack) Pop() {
+	this.stack = this.stack[:len(this.stack)-1]
 }
 
 func (this *MinStack) Top() int {
+	return this.stack[len(this.stack)-1]
 }
 
 func (this *MinStack) GetMin() int {
+	min := this.stack[0]
+	for _, v := range this.stack {
+		if v < min {
+			min = v
+		}
+	}
+	return min
 }
