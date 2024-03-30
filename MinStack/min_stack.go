16e9dbb170b4a0f0579128d3e47cc69f567d9de3
nt2311-vn <nguyentoan231196@gmail.com>
2024-03-30 14:41:48 +0700

    implement minstack problem

---
+++ MinStack/min_stack.go
@@ -0,0 +1,20 @@
+package min_stack
+
+type MinStack struct {
+	stack []int
+}
+
+func Constructor() {
+}
+
+func (this *MinStack) Push(val int) {
+}
+
+func (this *MinStack) Pop() {
+}
+
+func (this *MinStack) Top() int {
+}
+
+func (this *MinStack) GetMin() int {
+}
