86fba305996b88359fc73fe220c28d53fa2a64df
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-03 12:08:34 +0700

    complete queue solved implement

--- Student_Unable_Eat_Lunch/count_students.dart
+++ Student_Unable_Eat_Lunch/count_students.dart
@@ -1,22 +1,79 @@
-class Solution {
-  int countStudents(List<int> students, List<int> sandwiches) {
-    int s = sandwiches.length;
-    while (students.isNotEmpty && s > 0) {
-      bool found = false;
-      for (int i = 0; i < students.length; i++) {
-        if (students[i] == sandwiches[s]) {
-          students.removeAt(i);
-          s--;
-          found = true;
-          break;
-        }
-      }
+class Node {
+  int val;
+  Node? next;
 
-      if (s == 0) {
-        break;
-      }
+  Node(this.val, this.next);
+}
+
+class Queue {
+  Node? head;
+  Node? tail;
+  int size = 0;
+  Queue(this.head, this.tail, this.size);
+
+  bool isEmpty() {
+    return size == 0;
+  }
+
+  void enQueue(int val) {
+    Node node = Node(val, null);
+    if (isEmpty()) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      this.tail!.next = node;
+      this.tail = node;
+    }
+    this.size++;
+  }
+
+  Node? deQueue() {
+    if (isEmpty()) {
+      return null;
     }
 
-    return students.length;
+    Node removed = this.head!;
+    this.head = this.head!.next;
+
+    if (this.head == null) {
+      this.tail == null;
+    }
+    this.size--;
+    return removed;
+  }
+
+  int? front() {
+    if (isEmpty()) {
+      return null;
+    }
+
+    return this.head!.val;
+  }
+}
+
+class Solution {
+  int countStudents(List<int> students, List<int> sandwiches) {
+    Queue studentQueue = Queue(null, null, 0);
+
+    for (var student in students) {
+      studentQueue.enQueue(student);
+    }
+
+    for (var sandwich in sandwiches) {
+      int maxAttempts = studentQueue.size;
+
+      while (maxAttempts > 0 && studentQueue.front() != sandwich) {
+        Node? removed = studentQueue.deQueue();
+        studentQueue.enQueue(removed!.val);
+        maxAttempts--;
+      }
+
+      if (maxAttempts == 0) {
+        break;
+      }
+      studentQueue.deQueue();
+    }
+
+    return studentQueue.size;
   }
 }
