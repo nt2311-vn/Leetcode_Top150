9d5a67a5f4edd2af86b7cea56cbd7f408b9cf29c
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-03 10:56:44 +0700

    solved with dart

--- Student_Unable_Eat_Lunch/count_students.dart
+++ Student_Unable_Eat_Lunch/count_students.dart
@@ -1,82 +1,22 @@
-class Node {
-  int val;
-  Node? next;
-  Node(this.val, this.next);
-}
-
-class Queue {
-  Node? head;
-  Node? tail;
-  int size;
-  Queue(this.head, this.tail, this.size);
-
-  bool IsEmpty() {
-    return size == 0;
-  }
-
-  void Enqueue(int val) {
-    if (IsEmpty()) {
-      Node node = Node(val, null);
-      head = node;
-      tail = node;
-    } else {
-      Node node = Node(val, null);
-      tail?.next = node;
-      tail = node;
-    }
-    size++;
-  }
-
-  Node? Dequeue() {
-    if (IsEmpty()) {
-      throw Exception("Queue is empty, cannot dequeue");
-    }
-    Node removed = head!;
-    head = head!.next;
-
-    if (head == null) {
-      tail = null;
-    }
-    size--;
-    return removed;
-  }
-
-  int Front() {
-    if (IsEmpty()) {
-      throw Exception("Cannot return value of an empty queue");
-    }
-
-    return head!.val;
-  }
-}
-
 class Solution {
   int countStudents(List<int> students, List<int> sandwiches) {
-    Queue studentQueue = Queue(null, null, students.length);
-
-    for (var student in students) {
-      studentQueue.Enqueue(student);
-    }
-
-    for (var sandwich in sandwiches) {
-      int maxAttempt = students.length;
-
-      while (maxAttempt > 0 && studentQueue.Front() != sandwich) {
-        Node? removed = studentQueue.Dequeue();
-
-        if (removed != null) {
-          studentQueue.Enqueue(removed.val);
+    int s = 0;
+    while (students.isNotEmpty && s < sandwiches.length) {
+      bool found = false;
+      for (int i = 0; i < students.length; i++) {
+        if (students[i] == sandwiches[s]) {
+          students.removeAt(i);
+          s++;
+          found = true;
+          break;
         }
-        maxAttempt--;
       }
 
-      if (maxAttempt == 0) {
+      if (!found) {
         break;
       }
-
-      studentQueue.Dequeue();
     }
 
-    return studentQueue.size;
+    return students.length;
   }
 }
