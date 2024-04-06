2ca7a8afaa74d06307cab26fdd19273ed7b7ef19
ntEOS <nguyentoan231196@gmail.com>
2024-04-06 22:17:51 +0700

    try solved insertiton sort

---
+++ Insertion_Sort/insert_sort.js
@@ -0,0 +1,18 @@
+class Pair {
+	/**
+	 * @param {number} key - The key to be stored in the pair
+	 * @param {numbeer} value - The value to be stored in the pair
+	 */
+	constructor(key, value) {
+		this.key = key;
+		this.value = value;
+	}
+}
+
+class Solution {
+	/**
+	 * @param {Pair[]} pairs
+	 * @returns {Pair[][]}
+	 */
+	insertionSort(pairs) {}
+}
