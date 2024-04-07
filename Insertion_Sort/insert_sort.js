b077714e4bbd77009d4c47561832661fab42af9c
ntEOS <nguyentoan231196@gmail.com>
2024-04-07 10:07:48 +0700

    solved with js

--- Insertion_Sort/insert_sort.js
+++ Insertion_Sort/insert_sort.js
@@ -1,7 +1,7 @@
 class Pair {
 	/**
 	 * @param {number} key - The key to be stored in the pair
-	 * @param {numbeer} value - The value to be stored in the pair
+	 * @param {number} value - The value to be stored in the pair
 	 */
 	constructor(key, value) {
 		this.key = key;
@@ -11,8 +11,20 @@ class Pair {
 
 class Solution {
 	/**
-	 * @param {Pair[]} pairs
-	 * @returns {Pair[][]}
+	 * @param {Pair[]} pairs - The tuples of key, value pairs
+	 * @returns {Pair[][]} returns the order of each sort process
 	 */
-	insertionSort(pairs) {}
+	insertionSort(pairs) {
+		const results = [];
+		for (let i = 0; i < pairs.length; i++) {
+			let j = i - 1;
+			while (j >= 0 && pairs[j + 1].key < pairs[j].key) {
+				[pairs[j], pairs[j + 1]] = [pairs[j + 1], pairs[j]];
+				j--;
+			}
+			results.push([...pairs]);
+		}
+
+		return results;
+	}
 }
