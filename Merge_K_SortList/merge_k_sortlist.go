62cee8b93fce2b6d25f38545498039692d6ba79e
ntEOS <nguyentoan231196@gmail.com>
2024-04-07 21:05:04 +0700

    solved hard merge k list with go using merge sort

---
+++ Merge_K_SortList/merge_k_sortlist.go
@@ -0,0 +1,45 @@
+package mergeksortlist
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func mergeKLists(lists []*ListNode) *ListNode {
+	if len(lists) == 0 {
+		return nil
+	}
+
+	if len(lists) == 1 {
+		return lists[0]
+	}
+
+	mid := len(lists) / 2
+
+	left, right := mergeKLists(lists[:mid]), mergeKLists(lists[mid:])
+
+	return merge(left, right)
+}
+
+func merge(l1, l2 *ListNode) *ListNode {
+	dummy := &ListNode{}
+	curr := dummy
+
+	for l1 != nil && l2 != nil {
+		if l1.Val < l2.Val {
+			curr.Next = l1
+			l1 = l1.Next
+		} else {
+			curr.Next = l2
+			l2 = l2.Next
+		}
+		curr = curr.Next
+	}
+	if l1 != nil {
+		curr.Next = l1
+	} else {
+		curr.Next = l2
+	}
+
+	return dummy.Next
+}
