c19d3800b865de73cf55ece00e45219367f16e84
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-09 14:24:38 +0700

    solved with go

---
+++ Reverse_Words/reverse_words.go
@@ -0,0 +1,13 @@
+package reversewords
+
+import "strings"
+
+func reverseWords(s string) string {
+	words := strings.Fields(s)
+
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	return strings.Join(words, " ")
+}
