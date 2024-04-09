e8f3c5f1c6f30cf8a74fab6723f6c1ebedad5d27
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-09 15:25:25 +0700

    solved with js

---
+++ Index_First_Occurrence/first_occurrence.js
@@ -0,0 +1,8 @@
+/**
+ * @param {string} haystack - larger string
+ * @param {string} needle - smaller string
+ * @returns {number} returns the first index occur
+ */
+var strStr = function (haystack, needle) {
+	return haystack.indexOf(needle);
+};
