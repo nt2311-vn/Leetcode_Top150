4d00317bfce26572a062e21a6e45e535a0b6790a
ntEOS <nguyentoan231196@gmail.com>
2024-04-13 15:59:04 +0700

    solved with js

---
+++ Pow/pow.js
@@ -0,0 +1,8 @@
+/**
+ * @param {number} x - the base number
+ * @param {number} n - the power number
+ * @returns {number} results of x power of n
+ */
+var myPow = function (x, n) {
+	return Math.pow(x, n);
+};
