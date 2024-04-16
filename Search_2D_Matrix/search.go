ccbda5c866fefd9c67b95234e737934b2ffdb8c8
ntEOS <nguyentoan231196@gmail.com>
2024-04-16 23:26:07 +0700

    solved with go

---
+++ Search_2D_Matrix/search.go
@@ -0,0 +1,21 @@
+package search2dmatrix
+
+func searchMatrix(matrix [][]int, target int) bool {
+	if len(matrix) == 0 || len(matrix[0]) == 0 {
+		return false
+	}
+
+	row, col := 0, len(matrix[0])-1
+
+	for row < len(matrix) && col >= 0 {
+		if matrix[row][col] > target {
+			col--
+		} else if matrix[row][col] < target {
+			row++
+		} else {
+			return true
+		}
+	}
+
+	return false
+}
