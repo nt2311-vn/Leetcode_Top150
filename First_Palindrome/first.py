170229348be0ef329b91455efa5a7ada86eb30dd
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-16 17:58:27 +0700

    solved with python

---
+++ First_Palindrome/first.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def firstPalindrome(self, words: List[str]) -> str:
+        for word in words:
+            if word[::-1] == word:
+                return word
+        return ""
