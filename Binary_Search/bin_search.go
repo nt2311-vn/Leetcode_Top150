a3c8a5738e64332e4bf47a62382cda1c4934d9cf
ntEOS <nguyentoan231196@gmail.com>
2024-04-16 23:02:09 +0700

    solved with go

---
+++ Binary_Search/bin_search.go
@@ -0,0 +1,19 @@
+package binarysearch
+
+func search(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+
+	for l <= r {
+		m := (l + r) / 2
+
+		if nums[m] > target {
+			r = m - 1
+		} else if nums[m] < target {
+			l = m + 1
+		} else {
+			return m
+		}
+	}
+
+	return -1
+}
