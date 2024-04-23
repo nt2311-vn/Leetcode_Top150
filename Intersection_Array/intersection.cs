6512a60247513b72e4eb312a7194157f9fb5dab6
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-23 16:57:36 +0700

    add using system default methods

--- Intersection_Array/intersection.cs
+++ Intersection_Array/intersection.cs
@@ -1,25 +1,33 @@
 using System;
-public class Solution {
-    public int[] Intersection(int[] nums1, int[] nums2) {
+using System.Collections.Generic;
+
+public class Solution
+{
+    public int[] Intersection(int[] nums1, int[] nums2)
+    {
         HashSet<int> result = new HashSet<int>();
         Array.Sort(nums1);
         Array.Sort(nums2);
 
-        int i =0 , j =0;
+        int i = 0,
+            j = 0;
 
         while (i < nums1.Length && j < nums2.Length)
         {
-          if (nums1[i] == nums2[j])
-          {
-            result.Add(nums1[i]);
-            i++;
-            j++;
-          } else if (nums1[i] > nums2[j])
-          {
-           j++;   
-          } else {
-            i++;
-          }
+            if (nums1[i] == nums2[j])
+            {
+                result.Add(nums1[i]);
+                i++;
+                j++;
+            }
+            else if (nums1[i] > nums2[j])
+            {
+                j++;
+            }
+            else
+            {
+                i++;
+            }
         }
 
         return result.ToArray();
