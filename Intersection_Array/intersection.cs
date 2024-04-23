1de24b0f14ec22896ab1b6154555ec957fff67ad
nt2311-vn <nguyentoan231196@gmail.com>
2024-04-23 16:27:32 +0700

    solved with cs

---
+++ Intersection_Array/intersection.cs
@@ -0,0 +1,27 @@
+using System;
+public class Solution {
+    public int[] Intersection(int[] nums1, int[] nums2) {
+        HashSet<int> result = new HashSet<int>();
+        Array.Sort(nums1);
+        Array.Sort(nums2);
+
+        int i =0 , j =0;
+
+        while (i < nums1.Length && j < nums2.Length)
+        {
+          if (nums1[i] == nums2[j])
+          {
+            result.Add(nums1[i]);
+            i++;
+            j++;
+          } else if (nums1[i] > nums2[j])
+          {
+           j++;   
+          } else {
+            i++;
+          }
+        }
+
+        return result.ToArray();
+    }
+}
