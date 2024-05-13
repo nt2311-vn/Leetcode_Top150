d2b22afffd87d538a7dba0998fecb09c15c7d6d5
nt2311-vn <nguyentoan231196@gmail.com>
2024-05-13 17:28:49 +0700

    update c# solution

--- Palindrome/palindrome.cs
+++ Palindrome/palindrome.cs
@@ -14,100 +14,35 @@ public class Solution
 {
     public bool IsPalindrome(ListNode head)
     {
-        ListNode middle = head;
-        ListNode fast = head;
+        var slow = head;
+        var fast = head;
+        ListNode prev = null;
 
-        while (fast != null)
+        while (fast != null && fast.next != null)
         {
-            middle = middle.next;
-            fast = fast.next;
-
-            if (fast.next != null)
-            {
-                fast = fast.next;
-            }
+            fast = fast.next.next;
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
         }
 
-        ListNode tail = null;
-
-        while (middle != null)
+        if (fast != null)
         {
-            ListNode next = middle.next;
-            middle.next = tail;
-            tail = middle;
-            middle = next;
+            slow = slow.next;
         }
 
-        while (tail != null)
+        while (slow != null)
         {
-            if (head.val != tail.val)
+            if (slow.val != prev.val)
             {
-                return false;
+                return fals;
             }
+
+            slow = slow.next;
+            prev = prev.next;
         }
 
         return true;
-    }
-
-    public class ListNode
-    {
-        public int val;
-        public ListNode next;
-
-        public ListNode(int val = 0, ListNode next = null)
-        {
-            this.val = val;
-            this.next = next;
-        }
-    }
-
-    public class Solution
-    {
-        public bool IsPalindrome(ListNode head)
-        {
-            ListNode middle = head;
-            ListNode fast = head;
-
-            while (fast != null)
-            {
-                middle = middle.next;
-                fast = fast.next;
-
-                if (fast.next != null)
-                {
-                    fast = fast.next;
-                }
-            }
-
-            ListNode tail = ReverseList(middle);
-
-            while (tail != null)
-            {
-                if (head.val != tail.val)
-                {
-                    return false;
-                }
-                head = head.next;
-                tail = tail.next;
-            }
-
-            return true;
-        }
-
-        private ListNode ReverseList(ListNode head)
-        {
-            ListNode prev = null;
-            ListNode curr = head;
-
-            while (curr != null)
-            {
-                ListNode nextNode = curr.Next;
-                curr.Next = prev;
-                prev = curr;
-                curr = nextNode;
-            }
-
-            return prev;
-        }
     }
 }
